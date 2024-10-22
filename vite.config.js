0ba823a75f63b4f721839a571bc92aba95de5677
Amin Ansari Mehr <aminansarimehrgeek23@gmail.com>
2024-10-22 15:31:11 +0330

    install sass package & configuration vite.config.js file

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+      "@assets": path.resolve(__dirname, "./src/assets"),
+      "@scss": path.resolve(__dirname, "./src/assets/scss"),
+      "@decoration": path.resolve(__dirname, "./src/assets/decorations"),
+      "@components": path.resolve(__dirname, "./src/components"),
+    },
+  },
   plugins: [vue()],
-})
+});
