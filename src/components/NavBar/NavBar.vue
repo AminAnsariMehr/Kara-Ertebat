9207abd696b34cfbc3f674f19be71c5b6691375d
Amin Ansari Mehr <aminansarimehrgeek23@gmail.com>
2024-10-23 22:24:32 +0330

    start & layout navbar elements

--- src/components/NavBar/NavBar.vue
+++ src/components/NavBar/NavBar.vue
@@ -1,6 +1,31 @@
-<style src="" lang="scss" scoped></style>
+<style src="./NavBar.scss" lang="scss" scoped></style>
 
-<template></template>
+<template>
+  <nav class="navigationBar">
+    <ul class="menu">
+      <li class="menu__menuItems">
+        <i class="menu__menuIcons">a</i>
+        <span href="#" class="menu__linkItems">خانه</span>
+      </li>
+      <li class="menu__menuItems">
+        <i class="menu__menuIcons">a</i>
+        <span href="#" class="menu__linkItems">تولید کنندگان</span>
+      </li>
+      <li class="menu__menuItems">
+        <i class="menu__menuIcons">a</i>
+        <span href="#" class="menu__linkItems">آگهی جدید</span>
+      </li>
+      <li class="menu__menuItems">
+        <i class="menu__menuIcons">a</i>
+        <span href="#" class="menu__linkItems">نمایشگاه</span>
+      </li>
+      <li class="menu__menuItems">
+        <i class="menu__menuIcons">a</i>
+        <span href="#" class="menu__linkItems">پروفایل</span>
+      </li>
+    </ul>
+  </nav>
+</template>
 
 <script>
 export default {};
