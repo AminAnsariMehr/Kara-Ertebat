980c2720a96e6ee8f4272547b4f4c4cb42c551c9
Amin Ansari Mehr <aminansarimehrgeek23@gmail.com>
2024-10-24 22:09:40 +0330

    fix header box shadow

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,6 @@
 
 <template>
   <div class="container">
-    <statusBar-component></statusBar-component>
     <header-component></header-component>
     <searchBar-component></searchBar-component>
     <categoryBar-component></categoryBar-component>
@@ -12,7 +11,7 @@
 </template>
 
 <script>
-import StatusBar from "@components/StatusBar/StatusBar.vue";
+// import StatusBar from "@components/StatusBar/StatusBar.vue";
 import Header from "@/components/Header/Header.vue";
 import SearchBar from "@/components/SearchBar/SearchBar.vue";
 import CategoryBar from "@/components/CategoryBar/CategoryBar.vue";
@@ -20,7 +19,7 @@ import Posts from "@/components/Posts/Posts.vue";
 import NavBar from "@/components/NavBar/NavBar.vue";
 export default {
   components: {
-    "statusBar-component": StatusBar,
+    // "statusBar-component": StatusBar,
     "header-component": Header,
     "searchBar-component": SearchBar,
     "categoryBar-component": CategoryBar,
--- src/components/Header/Header.vue
+++ src/components/Header/Header.vue
@@ -2,23 +2,29 @@
 
 <template>
   <header class="header">
-    <div class="header__titleApp">
-      <i class="fi fi-rr-menu-burger header__burgerIcon"></i>
-      <h1 class="header__siteTitle">های ساخت</h1>
-    </div>
+    <statusBar-component></statusBar-component>
+    <div class="headerSeparator">
+      <div class="header__titleApp">
+        <i class="fi fi-rr-menu-burger header__burgerIcon"></i>
+        <h1 class="header__siteTitle">های ساخت</h1>
+      </div>
 
-    <div class="header__userPanel">
-      <i class="fi fi-rs-bell header__notificationIcon"></i>
-      <div class="header__locationData">
-        <i class="fi fi-rr-marker header__locationIcon"></i>
-        <span class="header__locationName">تهران</span>
+      <div class="header__userPanel">
+        <i class="fi fi-rs-bell header__notificationIcon"></i>
+        <div class="header__locationData">
+          <i class="fi fi-rr-marker header__locationIcon"></i>
+          <span class="header__locationName">تهران</span>
+        </div>
       </div>
     </div>
   </header>
 </template>
 
 <script>
+import StatusBar from "@components/StatusBar/StatusBar.vue";
 export default {
-  components: {},
+  components: {
+    "statusBar-component": StatusBar,
+  },
 };
 </script>
