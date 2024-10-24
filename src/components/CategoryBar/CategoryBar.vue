0a9c23b80c40bb42d930d6e86e31e214b723335b
Amin Ansari Mehr <aminansarimehrgeek23@gmail.com>
2024-10-24 19:19:45 +0330

    finish posts components

--- src/components/CategoryBar/CategoryBar.vue
+++ src/components/CategoryBar/CategoryBar.vue
@@ -1,80 +1,82 @@
 <style src="./CategoryBar.scss" lang="scss" scoped></style>
 
 <template>
-  <ul class="categoryBar">
-    <li class="categoryBar__categoryItem categoryBar__categoryItem--selected">
-      <div class="categoryBar__imageWrapper">
-        <img
-          class="categoryBar__image"
-          src="./../../assets/decorations/loole.jpeg"
-        />
-      </div>
-      <span class="categoryBar__itemTitle">آهن آلات</span>
-    </li>
-    <li class="categoryBar__categoryItem">
-      <div class="categoryBar__imageWrapper">
-        <img
-          class="categoryBar__image"
-          src="./../../assets/decorations/cement.jpg"
-        />
-      </div>
-      <span class="categoryBar__itemTitle">گچ و سیمان</span>
-    </li>
-    <li class="categoryBar__categoryItem">
-      <div class="categoryBar__imageWrapper">
-        <img
-          class="categoryBar__image"
-          src="./../../assets/decorations/steel.jpg"
-        />
-      </div>
-      <span class="categoryBar__itemTitle">آلومینیوم</span>
-    </li>
-    <li class="categoryBar__categoryItem">
-      <div class="categoryBar__imageWrapper">
-        <img
-          class="categoryBar__image"
-          src="./../../assets/decorations/rock.jpeg"
-        />
-      </div>
-      <span class="categoryBar__itemTitle">انواع سنگ</span>
-    </li>
-    <li class="categoryBar__categoryItem">
-      <div class="categoryBar__imageWrapper">
-        <img
-          class="categoryBar__image"
-          src="./../../assets/decorations/tumber.jpg"
-        />
-      </div>
-      <span class="categoryBar__itemTitle">چوب و تخته</span>
-    </li>
-    <li class="categoryBar__categoryItem">
-      <div class="categoryBar__imageWrapper">
-        <img
-          class="categoryBar__image"
-          src="./../../assets/decorations/tumber.jpg"
-        />
-      </div>
-      <span class="categoryBar__itemTitle">چوب و تخته</span>
-    </li>
-    <li class="categoryBar__categoryItem">
-      <div class="categoryBar__imageWrapper">
-        <img
-          class="categoryBar__image"
-          src="./../../assets/decorations/tumber.jpg"
-        />
-      </div>
-      <span class="categoryBar__itemTitle">چوب و تخته</span>
-    </li>
-    <li class="categoryBar__categoryItem">
-      <div class="categoryBar__imageWrapper">
-        <img
-          class="categoryBar__image"
-          src="./../../assets/decorations/tumber.jpg"
-        />
-      </div>
-      <span class="categoryBar__itemTitle">چوب و تخته</span>
-    </li>
-  </ul>
+  <div class="postsWrapper">
+    <ul class="categoryBar">
+      <li class="categoryBar__categoryItem categoryBar__categoryItem--selected">
+        <div class="categoryBar__imageWrapper">
+          <img
+            class="categoryBar__image"
+            src="./../../assets/decorations/loole.jpeg"
+          />
+        </div>
+        <span class="categoryBar__itemTitle">آهن آلات</span>
+      </li>
+      <li class="categoryBar__categoryItem">
+        <div class="categoryBar__imageWrapper">
+          <img
+            class="categoryBar__image"
+            src="./../../assets/decorations/cement.jpg"
+          />
+        </div>
+        <span class="categoryBar__itemTitle">گچ و سیمان</span>
+      </li>
+      <li class="categoryBar__categoryItem">
+        <div class="categoryBar__imageWrapper">
+          <img
+            class="categoryBar__image"
+            src="./../../assets/decorations/steel.jpg"
+          />
+        </div>
+        <span class="categoryBar__itemTitle">آلومینیوم</span>
+      </li>
+      <li class="categoryBar__categoryItem">
+        <div class="categoryBar__imageWrapper">
+          <img
+            class="categoryBar__image"
+            src="./../../assets/decorations/rock.jpeg"
+          />
+        </div>
+        <span class="categoryBar__itemTitle">انواع سنگ</span>
+      </li>
+      <li class="categoryBar__categoryItem">
+        <div class="categoryBar__imageWrapper">
+          <img
+            class="categoryBar__image"
+            src="./../../assets/decorations/timber.jpg"
+          />
+        </div>
+        <span class="categoryBar__itemTitle">چوب و تخته</span>
+      </li>
+      <li class="categoryBar__categoryItem">
+        <div class="categoryBar__imageWrapper">
+          <img
+            class="categoryBar__image"
+            src="./../../assets/decorations/timber.jpg"
+          />
+        </div>
+        <span class="categoryBar__itemTitle">چوب و تخته</span>
+      </li>
+      <li class="categoryBar__categoryItem">
+        <div class="categoryBar__imageWrapper">
+          <img
+            class="categoryBar__image"
+            src="./../../assets/decorations/timber.jpg"
+          />
+        </div>
+        <span class="categoryBar__itemTitle">چوب و تخته</span>
+      </li>
+      <li class="categoryBar__categoryItem">
+        <div class="categoryBar__imageWrapper">
+          <img
+            class="categoryBar__image"
+            src="./../../assets/decorations/timber.jpg"
+          />
+        </div>
+        <span class="categoryBar__itemTitle">چوب و تخته</span>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
--- src/components/Posts/Posts.vue
+++ src/components/Posts/Posts.vue
@@ -1,9 +1,38 @@
 <style src="./Posts.scss" lang="scss" scoped></style>
 
 <template>
-  <div class="posts"></div>
+  <div class="posts">
+    <div class="pastWrapper" v-for="post in postImages" :key="post">
+      <img class="pastWrapper__image" :src="post" alt="productPost" />
+    </div>
+  </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      postImages: [
+        "src/assets/decorations/arch.webp",
+        "src/assets/decorations/architecture.jpeg",
+        "src/assets/decorations/iron.jpeg",
+        "src/assets/decorations/engineer2.jpeg",
+        "src/assets/decorations/timber.jpg",
+        "src/assets/decorations/wood.jpg",
+        "src/assets/decorations/engineer.jpeg",
+        "src/assets/decorations/wood2.jpg",
+        "src/assets/decorations/loole.jpeg",
+        "src/assets/decorations/architecture.jpeg",
+        "src/assets/decorations/iron.jpeg",
+        "src/assets/decorations/engineer2.jpeg",
+        "src/assets/decorations/timber.jpg",
+        "src/assets/decorations/wood.jpg",
+        "src/assets/decorations/engineer.jpeg",
+        "src/assets/decorations/wood2.jpg",
+        "src/assets/decorations/loole.jpeg",
+        "src/assets/decorations/arch.webp",
+      ],
+    };
+  },
+};
 </script>
