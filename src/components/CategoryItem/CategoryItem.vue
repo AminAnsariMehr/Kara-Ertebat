d13c3ac54ed3a90d898ac7f5bb1f25f11db922c6
Amin Ansari Mehr <aminansarimehrgeek23@gmail.com>
2024-10-25 22:31:06 +0330

    create CategoryItem Component Files

---
+++ src/components/CategoryItem/CategoryItem.vue
@@ -0,0 +1,25 @@
+<style src="./CategoryBarItem.scss" lang="scss" scoped></style>
+<template>
+  <li
+    class="categoryBar__categoryItem"
+    :class="{ 'categoryBar__categoryItem--selected': isActive }"
+    :id="product.id"
+    @click="clickHandler"
+  >
+    <div class="categoryBar__imageWrapper">
+      <img class="categoryBar__image" :src="product.img" />
+    </div>
+    <span class="categoryBar__itemTitle">{{ product.title }}</span>
+  </li>
+</template>
+
+<script>
+export default {
+  props: ["product", "onClick", "isActive"],
+  methods: {
+    clickHandler() {
+      this.onClick(this.product.id);
+    },
+  },
+};
+</script>
